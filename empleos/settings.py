a8fda07a5435698a500ff12b4c4cb3ce58c593af
casvsv <cesar.salazar@unl.edu.ec>
2020-03-11 14:12:51 -0500

    Ahora si creo que final

--- empleos/settings.py
+++ empleos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,6 +91,7 @@ DATABASES = {
     }
 }
 
+LOGIN_URL=reverse_lazy('autenticar');
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
